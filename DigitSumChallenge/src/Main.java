e9be2fce9d130707b57f8f8730386372163e05f0
Jakub Fornalski <jakub.fornalski7@gmail.com>
2023-04-06 11:25:23 +0200

    .

--- DigitSumChallenge/src/Main.java
+++ DigitSumChallenge/src/Main.java
@@ -1,14 +1,26 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("the sum of the digits in number 1234 is" + sumDigits(1234));
+        System.out.println("the sum of the digits in number -125 is" + sumDigits(-125));
+        System.out.println("the sum of the digits in number 4 is" + sumDigits(4));
+        System.out.println("the sum of the digits in number 32123 is" + sumDigits(32123));
+
     }
     public static int sumDigits(int number){
-      if(number <0){
-          return -1;
-        }
-      else{
-          int parse1 = number % 10;
-          number = number / 10;
-          
+      if(number < 0){
+          return  -1;
       }
+
+        int sum = 0;
+
+      while (number > 9) {
+          sum += (number % 10);
+          number = number / 10;
+
+      }
+      sum += number;
+          return sum;
+
     }
+
 }
