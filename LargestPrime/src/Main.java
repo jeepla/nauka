6f887e173a924f6ecc35a1e5dec7f9a39a685c7b
Jakub Fornalski <jakub.fornalski7@gmail.com>
2023-04-09 13:00:59 +0200

    Course Progress

---
+++ LargestPrime/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getLargestPrime(21));
+    }
+    public static int getLargestPrime(int number) {
+        if (number <=1) {
+            return -1;
+        }
+        for (int i = 2; i < number; i++) {
+            if ((number % i) == 0) {
+                number /= i;
+                i--;
+            }
+        }
+        return number;
+    }
+}
--- NumberToWords/src/Main.java
+++ NumberToWords/src/Main.java
@@ -1,5 +1,79 @@
 public class Main {
     public static void main(String[] args) {
-       
+
     }
+public static int getDigitCount(int number){
+   if (number <0){
+       return -1;}
+       int counter = 1;
+       while (number / 10 !=0){
+          counter++;
+          number = number/10;
+       }
+       return counter;
 }
+
+public static int reverse(int number){
+        int lastDigit = 0;
+        int reverse = 0;
+    while (number / 10 !=0){
+        lastDigit= number%10;
+        reverse = (reverse *10) + lastDigit;
+        number = number / 10;
+    }
+    if(number%10 !=0){ //2
+        reverse = (reverse *10) + number;
+    }
+        return reverse;
+}
+
+
+
+    public static void numberToWords(int number) {
+        if (number < 0) {
+            System.out.println("Invalid Value");
+        }
+
+            int helper = reverse(number);
+            int lastDigit = 0;
+            int digit = getDigitCount(number);
+            for (int i = 0; i<digit; i++) {
+                lastDigit = helper %10;
+                switch(lastDigit) {
+                    case 0:
+                        System.out.print("Zero ");
+                        break;
+                    case 1:
+                        System.out.print("One ");
+                        break;
+                    case 2:
+                        System.out.print("Two ");
+                        break;
+                    case 3:
+                        System.out.print("Three ");
+                        break;
+                    case 4:
+                        System.out.print("Four ");
+                        break;
+                    case 5:
+                        System.out.print("Five ");
+                        break;
+                    case 6:
+                        System.out.print("Six ");
+                        break;
+                    case 7:
+                        System.out.print("Seven ");
+                        break;
+                    case 8:
+                        System.out.print("Eight ");
+                        break;
+                    case 9:
+                        System.out.print("Nine ");
+                        break;
+                    default:
+                        System.out.print("Invalid Value");
+                }
+                helper = helper /10;
+            }
+        }
+    }
