cdfdb9cd58216ff6476920b9e03b0fb8edcfd1c6
Jakub Fornalski <jakub.fornalski7@gmail.com>
2023-04-27 10:14:10 +0200

    Java Course Progress

---
+++ Bill_s_BurgersChallenge/src/DeluxeBurger.java
@@ -0,0 +1,28 @@
+public class DeluxeBurger extends Hamburger {
+
+    public DeluxeBurger() {
+        super("Deluxe", "Bacon & Cheese", 19.10, "Sesame Bun");
+        super.addHamburgerAddition1("Chips",0);
+        super.addHamburgerAddition2("Drink",0);
+    }
+
+    @Override
+    public void addHamburgerAddition1(String name, double price) {
+        System.out.println("no additional items can be added");
+    }
+
+    @Override
+    public void addHamburgerAddition2(String name, double price) {
+        System.out.println("no additional items can be added");
+    }
+
+    @Override
+    public void addHamburgerAddition3(String name, double price) {
+        System.out.println("no additional items can be added");
+    }
+
+    @Override
+    public void addHamburgerAddition4(String name, double price) {
+        System.out.println("no additional items can be added");
+    }
+}
